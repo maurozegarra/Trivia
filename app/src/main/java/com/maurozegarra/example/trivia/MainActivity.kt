15f81862c7a3a0ef374f5afe16b1f0b524cbce00
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-07-17 17:47:32 -0500

    Add Navigation Drawer Listener

--- app/src/main/java/com/maurozegarra/example/trivia/MainActivity.kt
+++ app/src/main/java/com/maurozegarra/example/trivia/MainActivity.kt
@@ -2,6 +2,7 @@ package com.maurozegarra.example.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -23,6 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
+            if (destination.id == controller.graph.startDestination)
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            else
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
